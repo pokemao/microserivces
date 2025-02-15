0c5591a71dbcce3e1458818f6b97178109729d05
yuzhe shao <william.yzshao@gmail.com>
2025-02-16 02:51:50 +0800

    chore(custom): ready to change npm registry source

--- servers/esbulid-config/utils.ts
+++ servers/esbulid-config/utils.ts
@@ -1,5 +1,5 @@
-import {rmSync} from "node:fs";
-import path, {resolve} from "node:path";
+import { rmSync } from "node:fs";
+import path, { resolve } from "node:path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,9 +8,9 @@ export const getDirname = () => {
   return __dirname;
 };
 
-export const asserts = (mode: string)  => {
+export const asserts = (mode: string) => {
   const lowerMode = mode.toLocaleLowerCase();
-  if(lowerMode === "production" || lowerMode === "development") {
+  if (lowerMode === "production" || lowerMode === "development") {
     return;
   } else {
     console.error("未知是开发模式还是生产模式");
@@ -19,12 +19,12 @@ export const asserts = (mode: string)  => {
 };
 
 export const clearBundle = (path: string) => {
-    const dir:string = resolve(__dirname, "../", path);
-    try {
-      rmSync(dir, { recursive: true, force: true });
-      console.log(`'${dir}' folder has been deleted.`);
-    } catch (error) {
-      console.error(`Error while deleting '${dir}': `, error);
-      process.exit(1);
-    }
+  const dir: string = resolve(__dirname, "../", path);
+  try {
+    rmSync(dir, { recursive: true, force: true });
+    console.log(`'${dir}' folder has been deleted.`);
+  } catch (error) {
+    console.error(`Error while deleting '${dir}': `, error);
+    process.exit(1);
+  }
 };
